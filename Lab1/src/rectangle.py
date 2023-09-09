c1d1084d974d8b34c70b2d528a2e732e35742684
Karena ma <karenama@karenas-mbp.lan>
2023-09-09 12:21:20 -0700

    Refactored rectangle class

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -1,25 +1,45 @@
 """
- Sample modified from CS5500, Mike Shah
+Sample modified from CS5500, Mike Shah
 
- A rectangle class
- Note that there is no constructor or destructor,
- so a default one will be created for us.
+A rectangle class
+Note that there is no constructor or destructor,
+so a default one will be created for us.
 """
-class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+from abc import ABC, abstractmethod
 
+class Shape(ABC):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    @abstractmethod
     def area(self):
-        return self.width * self.height
+        pass
 
+    def set_width(self, width):
+        self.__width = width
+
+    def get_width(self):
+        return self.__width
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_height(self):
+        return self.__height
+
+class Rectangle(Shape):
+    def area(self):
+        return self.get_width() * self.get_height()
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(3, 4)
 
     # Call a member function
-    rect.set_values(3, 4)
+    rect.set_width(3)
+    rect.set_height(4)
 
     # Print out the area function
     print("area:", rect.area())
+   
